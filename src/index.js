e7e1ca0355ec5fcb3f069de7ef20d39dd009b586
fmsieck <fmsieck@gmail.com>
2020-08-07 02:16:57 -0400

    first commit. NOT easy but concepts are beginning to make sense.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
-
 let todos = [
-    {
-        completed: false,
-        description: "Take Jim to the hair salon"
-    },
-    {
-        completed: true,
-        description: "Drop off wedding invitation at mailbox"
-    },
-    {
-        completed: false,
-        description: "Pick up the cake"
-    },
-    {
-        completed: false,
-        description: "Call the caterers"
-    }
+  {
+    completed: false,
+    description: "Take Jim to the hair salon",
+  },
+  {
+    completed: true,
+    description: "Drop off wedding invitation at mailbox",
+  },
+  {
+    completed: false,
+    description: "Pick up the cake",
+  },
+  {
+    completed: false,
+    description: "Call the caterers",
+  },
 ];
 
 function renderTodoApp() {
-    let app = document.querySelector("#app")
-    let h1 = document.createElement("h1")
-    // your code here
+  let app = document.querySelector("#app");
+  let h1 = document.createElement("h1");
+  let unorderedList = document.createElement("ul");
 
-    h1.innerText = "Todo List"
+  // your code here
 
-    app.appendChild(h1)
-    // and maybe some here
+  h1.innerText = "Todo List";
+
+  app.appendChild(h1);
+  h1.appendChild(unorderedList);
+  // and maybe some here
+  for (i = 0; i < todos.length; i++) {
+    let itemDescription = todos[i].description;
+    let itemCompleted = todos[i].completed ? "checked" : "";
+    let markUp = `
+        <li><input type="checkbox" ${itemCompleted}/>${itemDescription}
+        </li>
+        `;
+    unorderedList.innerHTML += markUp;
+  }
 }
-
-renderTodoApp()
+renderTodoApp();
