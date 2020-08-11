8177226ddeefe786ea22e60bf053ed4940b1dbf4
fmsieck <fmsieck@gmail.com>
2020-08-11 00:57:53 -0400

    updated javascript with working button.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let todos = [
     description: "Take Jim to the hair salon",
   },
   {
-    completed: true,
+    completed: false,
     description: "Drop off wedding invitation at mailbox",
   },
   {
@@ -17,18 +17,18 @@ let todos = [
   },
 ];
 
+let app = document.querySelector("#app");
+let h1 = document.createElement("h1");
+let unorderedList = document.createElement("ul");
+unorderedList.style.listStyleType = "none";
+
+h1.innerText = "Todo List";
+
+app.appendChild(h1);
+h1.appendChild(unorderedList);
+
 function renderTodoApp() {
-  let app = document.querySelector("#app");
-  let h1 = document.createElement("h1");
-  let unorderedList = document.createElement("ul");
-
-  // your code here
-
-  h1.innerText = "Todo List";
-
-  app.appendChild(h1);
-  h1.appendChild(unorderedList);
-  // and maybe some here
+  unorderedList.innerHTML = " ";
   for (index = 0; index < todos.length; index++) {
     let itemDescription = todos[index].description;
     let itemCompleted = todos[index].completed ? "checked" : "";
@@ -39,4 +39,16 @@ function renderTodoApp() {
     unorderedList.innerHTML += markUp;
   }
 }
+
+let inputElement = document.querySelector("#btninput");
+let buttonElement = document.querySelector("#btnsubmit");
+
+function onButtonClick() {
+  let toDoTask = { completed: false, description: inputElement.value };
+  todos.push(toDoTask);
+  renderTodoApp();
+}
+
+buttonElement.addEventListener("click", onButtonClick);
+
 renderTodoApp();
